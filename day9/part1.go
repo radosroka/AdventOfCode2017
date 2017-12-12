76e0f931cae0456804343a6545b5c7f2c4957a3e
Radovan Sroka <rsroka@redhat.com>
2017-12-12 19:17:42 +0100

    day9

---
+++ day9/part1.go
@@ -0,0 +1,87 @@
+package main
+
+import "fmt"
+
+var score int64 = 0
+
+func getFirst(str *string) (byte) {
+  var ch byte = 0
+  if len(*str) > 0 {
+    ch = (*str)[0]
+    *str = (*str)[1:]
+  }
+  return ch
+}
+
+func eatMess(str *string) {
+
+  var flag bool = true
+  for {
+    var x byte = getFirst(str)
+
+    if flag && x == '>' {
+      break
+    }
+
+    if flag && x == '!' {
+      flag = false
+      continue
+    }
+
+    if !flag {
+      flag = true
+    }
+
+  }
+}
+
+func recursive(str *string, r int64) {
+
+  if len(*str) <= 0 {
+    return
+  }
+
+  r += 1
+  score += r
+
+  // /fmt.Printf("recursive: %s | %d | %d \n", (*str), r, score)
+
+  var x byte = 0
+
+  for {
+    x = getFirst(str)
+    if x == 0 {
+      break
+    }
+
+  //  fmt.Printf("recursive loop: %s | %c \n", (*str), x)
+
+    switch x {
+      case '{':
+        recursive(str, r)
+        continue
+      case '}':
+        break
+      case ',':
+        continue
+      case '<':
+        eatMess(str)
+        continue
+      default:
+        break
+    }
+    break
+  }
+
+  //fmt.Printf("recursive_end: %s | %d | %d \n", (*str), r, score)
+}
+
+func main() {
+  var input string
+  var r int64 = -1
+  fmt.Scanln(&input)
+  //fmt.Println(input)
+  recursive(&input, r)
+
+  fmt.Println(score)
+}
